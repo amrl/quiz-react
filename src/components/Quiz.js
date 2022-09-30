8f8f9c85440cb164bbbf34f9ef35ed78383301fa
amrl <amirul.ardy@gmail.com>
2022-09-30 16:00:21 +0800

    Initialize base components

---
+++ src/components/Quiz.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Quiz() {
+    return (
+        <div className="quiz">
+            
+        </div>
+    );
+}
+
+export default Quiz
---
+++ src/components/Start.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Start({handleStartClick}) {
+    return (
+        <div className="start">
+            <h1 className="start--title">QuickQuiz</h1>
+            <h2 className="start--subtitle">A decent quiz game</h2>
+            <button
+                className="start--btn"
+                onClick={handleStartClick}
+            >Start quiz
+            </button>
+        </div>
+    );
+}
+
+export default Start;
